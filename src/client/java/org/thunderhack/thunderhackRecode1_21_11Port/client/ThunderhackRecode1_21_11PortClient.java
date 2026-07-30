5e3e7ad284bd5dc2a6ba7902233eaff24b01cf4f
Kostiantyn <90957529+Kocmo09@users.noreply.github.com>
2026-07-30 12:26:39 -0700

    skeleton

---
+++ src/client/java/org/thunderhack/thunderhackRecode1_21_11Port/client/ThunderhackRecode1_21_11PortClient.java
@@ -0,0 +1,10 @@
+package org.thunderhack.thunderhackRecode1_21_11Port.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThunderhackRecode1_21_11PortClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/thunderhack/thunderhackRecode1_21_11Port/ThunderhackRecode1_21_11Port.java
@@ -0,0 +1,10 @@
+package org.thunderhack.thunderhackRecode1_21_11Port;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ThunderhackRecode1_21_11Port implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
