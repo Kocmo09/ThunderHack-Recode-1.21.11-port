6fb3e10fe4df2a0bde8418dd8b7486b98276c2a9
Kostiantyn <90957529+Kocmo09@users.noreply.github.com>
2026-08-01 06:19:21 -0700

    sk

---
+++ src/main/java/java/thunderhack/client/thunderhackClient.java
@@ -0,0 +1,10 @@
+package thunderhack.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class thunderhackClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
